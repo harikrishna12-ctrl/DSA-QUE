55616c362d156a67584d492ff9aa52dac2689cfb
harikrishna <charikrishnareddy78995@gmail.com>
2026-06-23 07:22:23 +0530

    added full pyramid

--- PATTER PRINTING/pyramid.py
+++ PATTER PRINTING/pyramid.py
@@ -1,23 +1,26 @@
 n = int(input("Enter the number of rows: "))
 
-for i in range(2 * n - 1):
+ml=n//2+1
+nsp=n//2
+nst=1
 
-    if i < n:
-        for j in range(n - i - 1):
-            print(" ", end="")
+for i in range(1,n+1):
 
-        for j in range(2 * i + 1):
-            print("*", end="")
+    for j in range(nsp):
+        print(" ",end=" ")
 
-        print()
+
+    for j in range(nst):
+        print("*",end=" ")
+
+
+    if(i<ml):
+        nst=nst+2
+        nsp=nsp-1
+
 
     else:
-        z = i - n + 1
+        nsp=nsp+1
+        nst=nst-2
 
-        for j in range(z):
-            print(" ", end="")
-
-        for j in range(2 * (n - z) - 3):
-            print("*", end="")
-
-        print()
+    print()
