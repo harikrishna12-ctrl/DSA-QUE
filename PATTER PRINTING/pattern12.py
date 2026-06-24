b742b0be5d509aed51e0e03994ce212ade7dc198
harikrishna <charikrishnareddy78995@gmail.com>
2026-06-24 20:24:39 +0530

    added reverse printing ok abcd

--- PATTER PRINTING/pattern12.py
+++ PATTER PRINTING/pattern12.py
@@ -1,8 +1,19 @@
 n = int(input("enter the number : "))
 
 a = 64
+# for i in range(1, n):
+#     for j in range(1, i + 1):
+#         z = a + j
+#         print(chr(z), end=" ")
+#     print()
+
+x=n
+b=1
 for i in range(1, n):
-    for j in range(1, i + 1):
-        z = a + j
+    for j in range(n-i,0,-1):
+        z = a + b
+        b=b+1
         print(chr(z), end=" ")
+    x=x-1
+    b=1
     print()
