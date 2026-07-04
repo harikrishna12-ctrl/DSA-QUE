3429473f74a74469ffa28a0de76a211a7aef4b1b
harikrishna12-ctrl <charikrishnareddy78995@gmail.com>
2026-07-04 07:19:03 +0530

    Time: 0 ms (100%), Space: 13.2 MB (53.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,26 @@
 class Solution(object):
     def twoSum(self, nums, target):
-      ans =[]
-      for i in range(len(nums)):
-        for j in range(len(nums)-1,-1,-1):
-            if(i==j):
-                pass
-            else:
-                if(nums[i]+nums[j] ==target):
-                    ans.append(i)
-                    ans.append(j)
-                    break
-        if(len(ans)==2):
-            break
-      return ans
-        
+    #   ans =[]
+    #   for i in range(len(nums)):
+    #     for j in range(len(nums)-1,-1,-1):
+    #         if(i==j):
+    #             pass
+    #         else:
+    #             if(nums[i]+nums[j] ==target):
+    #                 ans.append(i)
+    #                 ans.append(j)
+    #                 break
+    #     if(len(ans)==2):
+    #         break
+    #   return ans
+      hash ={}
+      i =0
+      remaining = 0
+      n = len(nums)
+      for i in range(n):
+        remaining = target - nums[i]
+
+        if remaining in hash:
+            return i, hash[remaining]
+        if remaining not in hash:
+            hash[nums[i]] = i
