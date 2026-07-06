eb2df98babc4d38d0ab90cda452bd21782fdeaf9
harikrishna12-ctrl <charikrishnareddy78995@gmail.com>
2026-07-06 23:22:04 +0530

    Time: 5388 ms (5.01%), Space: 13.4 MB (16.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,26 +1,29 @@
 class Solution(object):
     def twoSum(self, nums, target):
-    #   ans =[]
-    #   for i in range(len(nums)):
-    #     for j in range(len(nums)-1,-1,-1):
-    #         if(i==j):
-    #             pass
-    #         else:
-    #             if(nums[i]+nums[j] ==target):
-    #                 ans.append(i)
-    #                 ans.append(j)
-    #                 break
-    #     if(len(ans)==2):
-    #         break
-    #   return ans
-      hash ={}
-      i =0
-      remaining = 0
-      n = len(nums)
-      for i in range(n):
-        remaining = target - nums[i]
+      ans =[]
+      for i in range(len(nums)):
+        for j in range(len(nums)-1,-1,-1):
+            if(i==j):
+                pass
+            else:
+                if(nums[i]+nums[j] ==target):
+                    ans.append(i)
+                    ans.append(j)
+                    break
+        if(len(ans)==2):
+            break
+      return ans
+    
 
-        if remaining in hash:
-            return i, hash[remaining]
-        if remaining not in hash:
-            hash[nums[i]] = i
+    # this is a optimal solution
+    #   hash ={}
+    #   i =0
+    #   remaining = 0
+    #   n = len(nums)
+    #   for i in range(n):
+    #     remaining = target - nums[i]
+
+    #     if remaining in hash:
+    #         return i, hash[remaining]
+    #     if remaining not in hash:
+    #         hash[nums[i]] = i
